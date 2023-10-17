eb53937e14210692da2f430d0c8c09057497443c
b-mat7 <121493355+b-mat7@users.noreply.github.com>
2023-10-17 11:50:35 +0200

    encryption, token creation

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,13 @@
 
 import express from 'express'
 
-import { encrypt } from '../middlewares/authMiddleware.js'
-import { register } from '../controllers/authController.js'
+import { encrypt } from '../middlewares/encryptMiddleware.js'
+import { getUsers, register } from '../controllers/authController.js'
 
 export const router = new express.Router()
 
 router.post("/register", encrypt, register)
+
+
+// TESTING
+router.get("/", getUsers)
