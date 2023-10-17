dd3d452b81fe11ef8e4d25ed2b7dd4051f58a7ae
Your Name <ali3ajob@gmail.com>
2023-10-17 13:57:20 +0200

    card controller, transaction controller

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -1,13 +1,27 @@
+import { ObjectId } from "bson"
 import { getDb } from "../utils/db.js"
 
-export const card = async (req, res) => {
+export const addCard = async (req, res) => {
   const db = await getDb()
-  const user = await db.collection("users").findOne(req.body)
+  const user = await db.collection("users").findOne({_id: new ObjectId(req.body.owner)})
 
   if (!user) {
     res.status(403).end()
   } else {
-    const cards = await db.collection("cards").find({ owner: user._id })
+    req.body.owner = new ObjectId(req.body.owner)
+    await db.collection("cards").insertOne(req.body)
+    res.end()
+  }
+}
+
+export const card = async (req, res) => {
+  const db = await getDb()
+  const user = await db.collection("users").findOne({_id: new ObjectId(req.body.owner)})
+
+  if (!user) {
+    res.status(403).end()
+  } else {
+    const cards = await db.collection("cards").find({ owner: user._id }).toArray()
     res.json(cards)
   }
 }
--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -2,14 +2,13 @@ import { ObjectId } from "bson"
 import { getDb } from "../utils/db.js"
 
 export const addTransaction = async (req, res) => {
-  console.log(req.body.owner)
   const db = await getDb()
-  const card = await db.collection("cards").findOne({_id: new ObjectId(req.body.owner)})
+  const card = await db.collection("cards").findOne({_id: new ObjectId(req.body.card_id)})
   
   if (!card) {
     res.status(403).end()
   } else {
-    req.body.owner = new ObjectId(req.body.owner)
+    req.body.card_id = new ObjectId(req.body.card_id)
     await db.collection("transactions").insertOne(req.body)
     res.end()
   }
@@ -17,12 +16,12 @@ export const addTransaction = async (req, res) => {
 
 export const transaction = async (req, res) => {
   const db = await getDb()
-  const card = await db.collection("cards").findOne({_id: new ObjectId(req.body._id)})
+  const card = await db.collection("cards").findOne({_id: new ObjectId(req.body.card_id)})
 
   if (!card) {
     res.status(403).end()
   } else {
-    const transactions = await db.collection("transactions").find({ owner: card._id}).toArray()
+    const transactions = await db.collection("transactions").find({ card_id: card._id}).toArray()
     res.json(transactions)
   }
 }
